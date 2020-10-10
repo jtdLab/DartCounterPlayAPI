a46a743932b4ef45387d81479dc2537882afa745
mrjosch <sanoj050499A1z26c3b2*>
2020-10-10 22:45:23 +0200

    implemented ThrowValidatorTest.java change languge lvl 9->11 updated ThrowValidator.java

--- src/main/java/dartServer/model/ThrowValidator.java
+++ src/main/java/dartServer/model/ThrowValidator.java
@@ -8,30 +8,27 @@ public class ThrowValidator {
         if (t == null) return false;
 
         if (t.getPoints() < 0 || t.getPoints() > 180) return false;
+        if (t.getPoints() == 0 && t.getDartsThrown() != 3) return false;
         if (t.getPoints() > pointsLeft) return false;
         if (List.of(163, 166, 169, 172, 173, 175, 176, 178, 179).contains(t.getPoints())) return false;
 
-        if (t.getDartsThrown() < 0 || t.getDartsThrown() > 3) return false;
+        if (t.getDartsThrown() < 1 || t.getDartsThrown() > 3) return false;
 
         if (t.getDartsOnDouble() < 0 || t.getDartsOnDouble() > 3) return false;
         if (t.getDartsOnDouble() > t.getDartsThrown()) return false;
-        if (t.getDartsOnDouble() > 0 && !isValidFinish(pointsLeft)) return false;
-        if (t.getDartsOnDouble() == 2 && (isThreeDartFinish(pointsLeft) && !isOneDartFinish(pointsLeft) && !isTwoDartFinish(pointsLeft)))
-            return false;
+        if (t.getDartsOnDouble() > 0 && !isThreeDartFinish(pointsLeft)) return false;
+        if (t.getDartsOnDouble() == 2 && (isThreeDartFinish(pointsLeft) && !isOneDartFinish(pointsLeft) && !isTwoDartFinish(pointsLeft))) return false;
         if (t.getDartsOnDouble() == 3 && !isOneDartFinish(pointsLeft)) return false;
+        if(pointsLeft == 2 && t.getDartsOnDouble() != t.getDartsThrown()) return false;
 
         return true;
-    }
-
-    public static boolean isValidFinish(int points) {
-        return isThreeDartFinish(points) || isTwoDartFinish(points) || isOneDartFinish(points);
     }
 
     /**
      * @param points
      * @return true if its possible to finish points with 3 darts false else
      */
-    private static boolean isThreeDartFinish(int points) {
+    public static boolean isThreeDartFinish(int points) {
         if (points < 2 || points > 170) return false;
         if (List.of(159, 162, 163, 165, 166, 168, 169).contains(points)) return false;
 
@@ -42,7 +39,7 @@ public class ThrowValidator {
      * @param points
      * @return true if its possible to finish points with 2 darts false else
      */
-    private static boolean isTwoDartFinish(int points) {
+    public static boolean isTwoDartFinish(int points) {
         if (points < 2 || points > 110) return false;
         if (List.of(99, 102, 103, 105, 106, 108, 109).contains(points)) return false;
 
@@ -53,7 +50,7 @@ public class ThrowValidator {
      * @param points
      * @return true if its possible to finish points with 1 darts false else
      */
-    private static boolean isOneDartFinish(int points) {
+    public static boolean isOneDartFinish(int points) {
         if (points < 2 || points > 50) return false;
         return points == 50 || (points <= 40 && points % 2 == 0);
     }
--- src/test/java/dartServer/model/ThrowValidatorTest.java
+++ src/test/java/dartServer/model/ThrowValidatorTest.java
@@ -1,14 +1,134 @@
 package dartServer.model;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ThrowValidatorTest {
 
     @Test
-    void isValidThrow() {
+    void isValidThrow1() {
+        assertFalse(ThrowValidator.isValidThrow(null, 501));
+
+        assertFalse(ThrowValidator.isValidThrow(new Throw(-1,0,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(181,0,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,0,2), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,0,1), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,0,3), 100));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,0,3), 100));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> isValidThrow2() {
+        return List.of(163, 166, 169, 172, 173, 175, 176, 178, 179).stream()
+                .map(points -> DynamicTest.dynamicTest("Test that a score of " + points + " is not valid", () -> {
+                    assertFalse(ThrowValidator.isValidThrow(new Throw(points,0,3), 501));
+                }));
     }
 
     @Test
-    void isValidFinish() {
+    void isValidThrow3() {
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,0, 0), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,0, 4), 501));
+
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,-1, 3), 40));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,4, 3), 40));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,3, 1), 40));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(100,1, 3), 190));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(170,2, 3), 170));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(170,3, 3), 170));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(110,3, 3), 110));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(2,2, 3), 2));
     }
+
+    @Test
+    void isValidThrowCustomCases() {
+
+        // VALID Throws
+
+        // standard throw
+        assertTrue(ThrowValidator.isValidThrow(new Throw(180,0,3),501));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(90,0,3),321));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(90,0,3),231));
+
+        // checkouts
+        // 3-dart-finish
+        assertTrue(ThrowValidator.isValidThrow(new Throw(0,3,3), 2));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(141,1,3),141));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(100,1,3),100));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(100,2,3),100));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(50,3,3),50));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(40,2,3),40));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(50,1,3),50));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(2,3,3),2));
+
+        // 2-dart-finish
+        assertTrue(ThrowValidator.isValidThrow(new Throw(100,1,2),100));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(100,2,2),100));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(50,2,2),50));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(40,1,2),40));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(50,1,2),50));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(3,1,2),3));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(2,2,2),2));
+
+        // 1-dart-finish
+        assertTrue(ThrowValidator.isValidThrow(new Throw(50,1,1),50));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(40,1,1),40));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(40,1,1),40));
+        assertTrue(ThrowValidator.isValidThrow(new Throw(2,1,1),2));
+
+
+        // INVALID THROWS
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,3,0), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,2,2), 2));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,2,2), 100));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,1,1), 2));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(0,1,1), 100));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(2,1,2), 2));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,3,0), 178));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(163,0,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(179,0,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,3,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(60,3,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(170,3,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(170,2,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,1,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,-10,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,10,3), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,1,-10), 501));
+        assertFalse(ThrowValidator.isValidThrow(new Throw(180,1,10), 501));
+        assertFalse(ThrowValidator.isValidThrow(null, 501));
+    }
+
+
+    @TestFactory
+    Stream<DynamicTest> isThreeDartFinish() {
+        return IntStream.rangeClosed(2,170).filter(num -> !(List.of(159, 162, 163, 165, 166, 168, 169).contains(num))).boxed()
+                .map(points -> DynamicTest.dynamicTest("Test that " + points + " is 3-dart finish", () -> {
+                    assertTrue(ThrowValidator.isThreeDartFinish(points));
+                }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest>  isTwoDartFinish() {
+        return IntStream.rangeClosed(2,110).filter(num -> !(List.of(99, 102, 103, 105, 106, 108, 109).contains(num))).boxed()
+                .map(points -> DynamicTest.dynamicTest("Test that " + points + " is 2-dart finish", () -> {
+                    assertTrue(ThrowValidator.isThreeDartFinish(points));
+                }));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> isOneDartFinish() {
+        return IntStream.concat(IntStream.rangeClosed(2,40).filter(num -> num%2 == 0), IntStream.of(50)).boxed()
+                .map(points -> DynamicTest.dynamicTest("Test that " + points + " is 1-dart finish", () -> {
+                    assertTrue(ThrowValidator.isOneDartFinish(points));
+                }));
+    }
+
 }
